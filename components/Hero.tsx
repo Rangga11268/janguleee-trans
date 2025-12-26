9aca85cbdee6f99fc5cc9d0a5a90c9d41f6a76ff
Darell Rangga <darrelrangga@gmail.com>
2025-12-26 19:47:10 +0700

    Minor Fix 2

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,9 +6,8 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="min-h-screen bg-white pt-40 pb-12 px-4 md:px-8 flex flex-col justify-center">
+    <section className="min-h-screen bg-white pt-20 pb-12 px-4 md:px-8 flex flex-col justify-center">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-12 gap-4 h-full">
-        {/* BIG TYPE BLOCK (Col-Span-8) */}
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
